7abf2189039b4547d60160f878d7ad99b5de8662
Josh Hall <joshhall1991@gmail.com>
2021-04-29 19:51:20 -0500

    another one

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,13 @@
 import React from "react"
 import './App.css';
-import LoginPage from './component/LoginPage';
-import axios from "axios"
+
 import AccountPage from "./component/AccountPage"
 
 function App() {
 
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  function postCreator(){
-    axios.post("/createPost")
-  }
-
   return (
     <div>
       <AccountPage></AccountPage>
-      <button onClick={postCreator}>click</button>
     </div>
   )}
 
