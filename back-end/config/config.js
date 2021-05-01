16e267da65fed4b79398c0e44dd7fb9dca21168c
Divi <73816974+divi4@users.noreply.github.com>
2021-05-01 15:53:59 +1000

    Added link to profile, added config.js and .env with adjustments

---
+++ back-end/config/config.js
@@ -0,0 +1,46 @@
+exports.creds = {
+    identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+
+    clientID: '',
+
+    clientSecret: '',
+
+    responseType: 'code id_token',
+
+    responseMode: 'form_post',
+
+    redirectUrl: 'http://localhost:3005/auth/openid/return',
+
+    allowHttpForRedirectUrl: true,
+
+    validateIssuer: false,
+
+    issuer: null,
+
+    passReqToCallback: false,
+
+    useCookieInsteadOfSession: false,
+
+    cookieEncryptionKeys: [
+      { 'key': '12345678901234567890123456789012', 'iv': '123456789012' },
+      { 'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl' }
+    ],
+
+    scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+
+    loggingLevel: false,
+
+    nonceLifetime: null,
+
+    nonceMaxAmount: 5,
+
+    clockSkew: null,
+  };
+
+  exports.destroySessionUrl = 'http://localhost:3005';
+
+  exports.useMongoDBSessionStore = false;
+
+  exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+
+  exports.mongoDBSessionMaxAge = 24 * 60 * 60;
