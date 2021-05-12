8be8c6d4ec673b9c52f11874af340d81e1604f0d
Divi <73816974+divi4@users.noreply.github.com>
2021-05-12 17:41:52 +1000

    Added post id check when liking

--- back-end/controllers/profile.js
+++ back-end/controllers/profile.js
@@ -21,15 +21,13 @@ module.exports = {
     },
     updateLike: async (req, res)=>{
         let currentLike = Number(req.body.like)
-        let hasLiked = await Imgs.findOne({_id:req.body.postIdFromJSFile})
-        if(await Imgs.find({ userLikes: { $in: [req.user.microsoftId] } }) == false){
+        if(await Imgs.findOne({_id:req.body.postIdFromJSFile}).find({ userLikes: { $in: [req.user.microsoftId] } }) == false){
           try{
               await Imgs.findOneAndUpdate({_id:req.body.postIdFromJSFile}, {
                 like: currentLike + 1,
                 $push: {userLikes: req.user.microsoftId},
               })
               res.json('Added Like')  // This line is needed to update page - Indirectly Fixes a Type Error?
-              await Imgs.save();
           }catch(err){
               console.log(err)
           }
@@ -41,7 +39,6 @@ module.exports = {
                 $pull: {userLikes: req.user.microsoftId},
               })
               res.json('Removed Like')  // This line is needed to update page - Indirectly Fixes a Type Error?
-              await Imgs.save()
           }catch(err){
               console.log(err)
           }
